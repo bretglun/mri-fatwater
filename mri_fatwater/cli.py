db34134c02814dac53451d5cbf140cfb88c19f4f
Johan Berglund <berglund.hpj@gmail.com>
2026-03-18 17:47:34 +0100

    CLI: replace optparse with argparse and improved help with example

--- mri_fatwater/cli.py
+++ mri_fatwater/cli.py
@@ -1,24 +1,56 @@
-import optparse
+import argparse
+import sys
 from mri_fatwater import fatwater, io
+from importlib.resources import files
+from pathlib import Path
+
+
+def get_example_param_files():
+    path = Path(files('mri_fatwater')).parent / 'configs'
+    param_files = {}
+    for par in ('data', 'algo', 'model'):
+        file = path / f'{par}Params.yml'
+        param_files[par] = file if file.is_file() else None
+    return param_files
 
 
 def main():
+    param_files = get_example_param_files()
+    example_hint = ''
+    if all(p is not None for p in param_files):
+        example_hint = f'''Example parameter files:\n{param_files['data']}\n{param_files['algo']}\n{param_files['model']}\n\nExample usage:\nfatwater -d {param_files['data']} -a {param_files['algo']} -m {param_files['model']} -o results'''
+        
     # Initiate command line parser
-    p = optparse.OptionParser()
-    p.add_option('--dataParamFile', '-d', default='',  type="string",
-                 help="File path of data parameter configuration file")
-    p.add_option('--algoParamFile', '-a', default='',  type="string",
-                 help="File path of algorithm parameter configuration file")
-    p.add_option('--modelParamFile', '-m', default='',  type="string",
-                 help="File path of model parameter configuration file")
-    p.add_option('--outDir', '-o', default='.',  type="string",
-                 help="Path to save the results")
+    parser = argparse.ArgumentParser(
+        prog='fatwater',
+        description='MRI fat/water separation of chemical shift encoded data',
+        epilog=example_hint,
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument('--dataParamFile', '-d', default=None,
+                        help="File path of data parameter configuration file")
+    parser.add_argument('--algoParamFile', '-a', default=None,
+                        help="File path of algorithm parameter configuration file")
+    parser.add_argument('--modelParamFile', '-m', default=None,
+                        help="File path of model parameter configuration file")
+    parser.add_argument('--outDir', '-o', default='.',
+                        help="Path to save the results")
 
     # Parse command line
-    options, arguments = p.parse_args()
-
-    results = fatwater.separate(data_param_file=options.dataParamFile, algo_param_file=options.algoParamFile, model_param_file=options.modelParamFile)
-    io.save(results, options.outDir)
+    args = parser.parse_args()
+    
+    # If no arguments provided, show help and exit
+    if len(sys.argv) == 1:
+        parser.print_help()
+        return 1
+    
+    results = fatwater.separate(
+        data_param_file=args.dataParamFile,
+        algo_param_file=args.algoParamFile,
+        model_param_file=args.modelParamFile
+    )
+    io.save(results, args.outDir)
 
     return 0
 
